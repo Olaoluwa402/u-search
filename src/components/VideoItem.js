24f5c2555443005dceb6a1bba364f06753d521f1
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2020-04-04 21:47:48 +0000

    styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './VideoItem.css';
 
 import {Grid, Typography, Paper} from '@material-ui/core';
 
 const VideoItem = ({video, onVideoSelect}) => {
 	return (
 		<Grid item xs={12}>
-			<Paper style={{display:'flex', alignItems:'center', cursor:'pointer'}} onClick={() => onVideoSelect(video)}>
+			<Paper className="video-items"  onClick={() => onVideoSelect(video)}>
 				<img style={{marginRight:'10px'}}  src={video.snippet.thumbnails.medium.url} alt="thumbnail"/>
 				<Typography variant="subtitle1"><b>{video.snippet.title}</b></Typography>
 			</Paper>
